c7ee64b3690b213113989fca708f5a63be4e0aa1
Jonatan Sandoval <jona.sandovalguerra@gmail.com>
2020-04-15 17:17:59 -0600

    orden carpetas

--- application/modules/catalogos/tipo_producto/tipo_productoModel.php
+++ application/modules/catalogos/tipo_producto/tipo_productoModel.php
@@ -5,6 +5,7 @@ class tipo_productoModel extends Model {
 		$this->tabla = 'opr_tipo_producto';
 		$this->id_tabla = 'id_tipo_producto';
 		
+		$this->campos['id_empresa'] = Session::get('id_empresa');
 		$this->campos['descripcion'] = '';
 		$this->campos['clave'] = '';
 		$this->campos['estado_registro'] = 'A';					
